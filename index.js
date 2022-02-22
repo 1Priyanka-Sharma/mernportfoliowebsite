8c943f4d7a3897d38a97f0b4701065b8315b53e0
Priyanka <priyankaqaz12345@gmail.com>
2022-02-22 20:49:10 +0530

    Started

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const app = express();
+
+const path = require("path");
+const logger = require("morgan");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(
+    "mongodb+srv://test:hellotaejim@cluster0.k4syx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => console.log("database connected"))
+  .catch((err) => console.log(err));
+
+const MSGModel = require("./public/msg");
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+
+// public static path-template engine
+const static_path = path.join(__dirname + '/public');
+app.use(express.static(static_path));
+app.set('view engine', 'hbs');
+
+// Routing
+app.get('', (req, res) => {
+  res.render("index");
+ })
+
+ app.post("/msg",  async function (req, res) {
+  const text  = req.body.name;
+console.log(text); 
+ const newMSG = new MSGModel({
+   text: text.toString(),
+ });
+
+ await newMSG.save();
+});
+
+app.listen(process.env.PORT || 3000);
+module.exports = app;
