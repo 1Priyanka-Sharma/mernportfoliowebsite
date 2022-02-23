d26d014b786233ce2e64fac6e1b3d59242d887eb
Priyanka <priyankaqaz12345@gmail.com>
2022-02-23 19:55:28 +0530

    Email sent

--- index.js
+++ index.js
@@ -6,19 +6,17 @@ const path = require("path");
 const logger = require("morgan");
 const cors = require("cors");
 const mongoose = require("mongoose");
-var nodemailer = require('nodemailer');
+const nodemailer = require('nodemailer');
+const MSGModel = require("./public/msg");
 
 const db = process.env.DATABASE;
 
 mongoose.connect(db, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-}
-)
+})
   .then(() => console.log("database connected"))
   .catch((err) => console.log(err));
-
-const MSGModel = require("./public/msg");
 
 app.use(logger("dev"));
 app.use(express.json());
@@ -35,48 +33,52 @@ app.get('', (req, res) => {
   res.render("index");
 })
 
+// POST Request
 app.post("/msg", async function (req, res) {
   try {
     const { name, email, message } = req.body;
     const newMSG = new MSGModel({
       name, email, message
     });
-
     await newMSG.save();
+
     res.render("index", { content: 'Thank You. Your response is recorded.' });
-
-
-var transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: 'kpriyas1256@gmail.com',
-    pass: 'gcnqqjjqledogqhm'
-  }
-});
-
-var mailOptions = {
-  from: 'kpriyas1256@gmail.com',
-  to: 'kpriyas1256@gmail.com',
-  subject: 'Sending Email using Node.js',
-  text: 'That was easy!'
-};
-
-transporter.sendMail(mailOptions, function(error, info){
-  if (error) {
-    console.log(error);
-  } else {
-    console.log('Email sent: ' + info.response);
-  }
-});
-
+    sendEmail(name, email, message);
   }
   catch (error) {
     console.log("Error" + error.message);
   }
 });
 
+function sendEmail(name, email, message) {
+
+  let receiverEmail = process.env.EMAIL_ID;
+  let password = process.env.PASSWORD;
+
+  let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: receiverEmail,
+      pass: password
+    }
+  });
+
+  let mainContent = 'Name: ' + name + '\n' + 'Email Id: ' + email + '\n' + 'Message: ' + message;
+
+  let mailOptions = {
+    from: receiverEmail,
+    to: receiverEmail,
+    subject: 'Email From Portfolio website',
+    text: mainContent
+  };
+
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error)
+      console.log(error);
+    else
+      console.log('Email sent: ' + info.response);
+  });
+}
+
 app.listen(process.env.PORT || 3000);
 module.exports = app;
-
-
-
